2b0d509b3637773e99d96b65a08c09eaf1dce74d
Alexander Rogalsky <alexander.rogalsky@yandex.ru>
2020-12-17 08:21:17 +0300

    Init repo

---
+++ src/js/sketch/_default/InstanceMesh.js
@@ -0,0 +1,48 @@
+import * as THREE from 'three';
+
+import vs from './glsl/InstanceMesh.vs';
+import fs from './glsl/InstanceMesh.fs';
+
+export default class InstanceMesh extends THREE.Mesh {
+  constructor() {
+    // Define Geometries
+    const geometry = new THREE.InstancedBufferGeometry();
+    const baseGeometry = new THREE.BoxBufferGeometry(10, 10, 10);
+
+    // Copy attributes of the base Geometry to the instancing Geometry
+    geometry.copy(baseGeometry);
+
+    // Define attributes of the instancing geometry
+    const num = 1000;
+    const ibaPositions = new THREE.InstancedBufferAttribute(new Float32Array(num * 3), 3);
+    for (var i = 0, ul = num; i < ul; i++) {
+      ibaPositions.setXYZ(i, 0, 0, 0);
+    }
+    geometry.setAttribute('iPosition', ibaPositions);
+
+    // Define Material
+    const material = new THREE.RawShaderMaterial({
+      uniforms: {
+        time: {
+          type: 'f',
+          value: 0
+        },
+      },
+      vertexShader: vs,
+      fragmentShader: fs,
+    });
+
+    // Create Object3D
+    super(geometry, material);
+    this.name = 'InstanceMesh';
+    this.frustumCulled = false;
+    this.isActive = false;
+  }
+  start() {
+    this.isActive = true;
+  }
+  update(time) {
+    if (this.isActive === false) return;
+    this.material.uniforms.time.value += time;
+  }
+}
---
+++ src/js/sketch/_default/Mesh.js
@@ -0,0 +1,35 @@
+import * as THREE from 'three';
+
+import vs from './glsl/Mesh.vs';
+import fs from './glsl/Mesh.fs';
+
+export default class Mesh extends THREE.Mesh {
+  constructor() {
+    // Define Geometry
+    const geometry = new THREE.BoxBufferGeometry(10, 10, 10);
+
+    // Define Material
+    const material = new THREE.RawShaderMaterial({
+      uniforms: {
+        time: {
+          type: 'f',
+          value: 0
+        },
+      },
+      vertexShader: vs,
+      fragmentShader: fs,
+    });
+
+    // Create Object3D
+    super(geometry, material);
+    this.name = 'Mesh';
+    this.isActive = false;
+  }
+  start() {
+    this.isActive = true;
+  }
+  update(time) {
+    if (this.isActive === false) return;
+    this.material.uniforms.time.value += time;
+  }
+}
